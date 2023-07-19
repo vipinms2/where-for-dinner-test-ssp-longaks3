2bd1526ce8f896f4dc66c9f044fd3eccdb6b52e9
vipinms2 <vipins2@vmware.com>
2023-07-18 17:00:04 -0700

    1689724803

--- where-for-dinner-ui/src/DiningSearches.js
+++ where-for-dinner-ui/src/DiningSearches.js
@@ -16,7 +16,7 @@ function DiningSearches(props)
         <div name="diningSearches" id="diningSearches">
           <div className="bannerHeader"> 
             <div className="bannerCenter">
-            <label className="searchesHeader">Submitted Searches And Results1689717603</label>
+            <label className="searchesHeader">Submitted Searches And Results1689724803</label>
             </div>      	
           </div>           
           {searches}
